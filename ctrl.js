8b0254f5ba1952a344dc0a40b7a9e0f0dd64c0ab
ihatecsv <me@drakeluce.com>
2017-06-16 16:57:13 -0300

    Added GPIO control to Node

--- ctrl.js
+++ ctrl.js
@@ -1,43 +1,22 @@
-var oled = require('oled-ssd1306-i2c');
-var font = require('oled-font-5x7');
- 
-var opts = {
-  width: 128, // screen width 
-  height: 32, // screen height 
-  address: 0x3C, // Pass I2C address of screen if it is not the default of 0x3C 
-  device: '/dev/i2c-1', // Pass your i2c device here if it is not /dev/i2c-1 
-  microview: false, // set to true if you have a microview display 
-};
- 
-var disp = new oled(opts);
+var rpio = require('rpio');
+
+rpio.open(11, rpio.OUTPUT, rpio.LOW);
+rpio.open(13, rpio.OUTPUT, rpio.LOW);
 
 var getTime = function(){
 	return Math.floor(new Date().getTime()/1000);
 }
 
-var systemState = {
-	state: 0,
-	sTime: getTime()
-}
-
-var updateScreen = function(data){
-	disp.setCursor(5, 5);
-	disp.writeString(font, 1, 'Cats and dogs are really cool animals, you know.', 1, true);
-}
-
 var controlLoop = function(){
-	systemState.state = 1;
-	systemState.sTime = getTime();
-	updateScreen();
-	console.log(systemState);
+	
 	setTimeout(function(){
-		systemState.state = 0;
-		systemState.sTime = getTime();
-		updateScreen();
-		console.log(systemState);
+		rpio.write(11, rpio.HIGH);
+		
 		setTimeout(function(){
+			rpio.write(11, rpio.LOW);
 			controlLoop();
 		}, 5000);
+		
 	}, 2000);
 }
 
